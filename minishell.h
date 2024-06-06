3685f5dbb9d11234e0a5fcf5d15c99007564d8bd
Mohamed Idbellaali <midbella@c3r4p6.1337.ma>
2024-06-06 14:27:03 +0100

    initail commit
    only some test file will be deleted later

---
+++ minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: midbella <midbella@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 14:01:08 by midbella          #+#    #+#             */
+/*   Updated: 2024/06/04 15:31:14 by midbella         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <signal.h>
+#endif
